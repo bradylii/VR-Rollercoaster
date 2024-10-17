7281ed36194660c050bbd0e818b87eeec26ed428
Brady Li <67674462+bradylii@users.noreply.github.com>
2024-10-17 15:11:37 -0400

    pickachu

--- Assets/WaypointMove.cs
+++ Assets/WaypointMove.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Oculus.Haptics;
 
 public class WaypointMove : MonoBehaviour
 {
-    private float speed = 0.1f;                // Speed of movement
+    private float speed;                // Speed of movement
     public GameObject[] waypoints;            // Array of waypoints to move towards
     public GameObject currentTarget;         // Current target waypoint
     private int currentIndex = 0;             // Index of the current waypoint
     public float stopDistance = 0.1f;         // Distance threshold to consider the waypoint reached
 
     public float[] speeds;
+
+    public HapticClip clip;
+    private HapticClipPlayer hapticPlayer;
+
+    public AudioSource audioSource;
 
     // Called once during run time
     void Start()
@@ -20,17 +26,24 @@ public class WaypointMove : MonoBehaviour
         {
             // Set first target to go to
             currentTarget = waypoints[currentIndex]; 
+            speed = speeds[currentIndex];
         }
+
+        hapticPlayer = new HapticClipPlayer(clip);
     }
 
     // Update is called every frame
     void Update()
     {
+        Debug.Log(speed);
         if (currentTarget != null)
         {
             if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
             {
                 speed += 10f;
+                hapticPlayer.Play(Controller.Left);
+                audioSource.Play();
+                Debug.Log("working");
             }
             if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
             {
@@ -47,12 +60,13 @@ public class WaypointMove : MonoBehaviour
             {
                 currentIndex++;
                 
+
                 if (currentIndex >= waypoints.Length)
                 {
                     currentIndex = 0;
-                    speed = speeds[currentIndex];
+                    
                 }
-
+                speed = speeds[currentIndex];
                 currentTarget = waypoints[currentIndex];
             }
         }
